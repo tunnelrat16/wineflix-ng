6ba998e84c136c558c29941efdf16890efa8aec7
tunnelrat16 <tjandmatt@hotmail.com>
2022-05-16 12:46:09 -0700

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import { LogoComponent } from './logo/logo.component';
+import { NavComponent } from './nav/nav.component';
+import { UserAvatarComponent } from './user-avatar/user-avatar.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogoComponent,
+    NavComponent,
+    UserAvatarComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
